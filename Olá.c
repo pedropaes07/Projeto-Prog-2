2a4f1a23eb6b002adde7d4008243d59e220722c1
DanielJarude <danieljarude@gmail.com>
2025-10-29 16:55:43 -0300

    Enhance Olá.c with game logic and comments
    Added initial game structure and comments for functionality.

--- Olá.c
+++ Olá.c
@@ -1,16 +1,71 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
 
-int main (){
-
-
-    printf ("Olá, bom dia mundo");
-
-
+/*Arquivo csv
+nome,dica1,dica2,dica3,dica4,dica5
+*/
 
 
+//Inves de escolher nivel de dificuldade o jogo progredir em nivel de fácil pra médio pra díficil
+
+/*70 perguntas
+fácil 7 dicas
 
 
-    printf ("\n");
+
+
+
+medio 5 dicas
+
+
+
+
+
+dificil 3 dicas
+pontos (anda no tabuleiro) = dicas - dicas usadas
+*/
+
+/*sortear jogador começa*/
+
+/*como vai aparecer as dicas
+clica 2 e aparece uma dica ====  contadordicasdenivel-- 
+*/
+
+
+/*como a pessoa vai responder
+clica 1 coloca pra resposta ("Sem Acento")
+compara resposta usuario com o csv resposta correta
+*/
+
+/*CRUD jogo
+quantos jogador
+nivel dificuldade*/
+
+/*Cadastro jogador [3-5]
+enum [fácil,medio,dificil]
+vetor [i == quantidade de jogadores] = [A,B,C,D]
+Jogador jogador;
+Sorteio de jogador // ou quem cadastrou primeiro joga primeiro
+Sorteia pergunta // dentro do codigo
+Aparecer a primeira dica
+    Duas opções:
+    1º aperta um botão pra receber mais uma dica -- if press x show dick
+    2° aperta outro botão pra responder -- 
+Se a pessoa errou , sorteia outra pergunta e passa a vez
+Se a pessoa acertou, quantidade de dicas usadas - dicastotaisdonivels
+*/
+
+/*typedef struct{
+    char nome[50];
+    //int pontos; - pode ser visivel ao jogador ou pode ser invisivel
+    enum ("Vermelho" = 1,"Azul","Verde","Roxo","Amarelo"); COR  
+
+    
+} Jogador;*/
+
+
+int main()    
+
     return 0;
 }
